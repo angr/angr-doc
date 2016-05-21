26506c15fe85b604f737c0027a64941fe3f09c3c
David Manouchehri <manouchehri@riseup.net>
2016-05-21 17:49:16 +0000

    Added a test() so @ltfish doesn't get mad at me.

--- examples/defcon2016quals_baby-re/solve.py
+++ examples/defcon2016quals_baby-re/solve.py
@@ -12,26 +12,32 @@ DigitalOcean is horrible for single threaded applications, I would highly sugges
 
 import angr
 
-proj = angr.Project('/defcon/baby-re',  load_options={'auto_load_libs': False})
+def main():
+	proj = angr.Project('/defcon/baby-re',  load_options={'auto_load_libs': False})
 
-path_group = proj.factory.path_group(threads=4) # Doesn't really help to have more threads, but whatever.
+	path_group = proj.factory.path_group(threads=4) # Doesn't really help to have more threads, but whatever.
 
-# If we get to 0x402941, "Wrong" is going to be printed out, so definitely avoid that.
-path_group.explore(find=0x40294b, avoid=0x402941) 
-# If you use anywhere before 0x40292c, angr won't have the flag to print out yet. So don't do that.
+	# If we get to 0x402941, "Wrong" is going to be printed out, so definitely avoid that.
+	path_group.explore(find=0x40294b, avoid=0x402941) 
+	# If you use anywhere before 0x40292c, angr won't have the flag to print out yet. So don't do that.
 
-print path_group.found[0].state.posix.dumps(1) # The flag is at the end.
-# The flag is: Math is hard!
+	print path_group.found[0].state.posix.dumps(1) # The flag is at the end.
+	# The flag is: 
 
-"""
-Note: There will be a bunch of warnings on your terminal that look like this.
+	"""
+	Note: There will be a bunch of warnings on your terminal that look like this.
 
-WARNING | 2016-05-21 17:34:33,185 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
-WARNING | 2016-05-21 17:34:49,353 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
-WARNING | 2016-05-21 17:35:11,810 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
-WARNING | 2016-05-21 17:35:44,170 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+	WARNING | 2016-05-21 17:34:33,185 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+	WARNING | 2016-05-21 17:34:49,353 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+	WARNING | 2016-05-21 17:35:11,810 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+	WARNING | 2016-05-21 17:35:44,170 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
 
-Don't worry about these, they're not an issue for this challenge.
-"""
+	Don't worry about these, they're not an issue for this challenge.
+	"""
+
+def test():
+	assert main() == 'Math is hard!'
 
 
+if __name__ == '__main__':
+	print(repr(main()))
