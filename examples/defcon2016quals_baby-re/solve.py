3fa0e6a96732ccd179d38f51032ef7fd0fe3893c
David Manouchehri <manouchehri@riseup.net>
2016-05-21 17:43:56 +0000

    Added solution.

---
+++ examples/defcon2016quals_baby-re/solve.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+"""
+Author: David Manouchehri <manouchehri@protonmail.com>
+DEFCON CTF Qualifier 2016
+Challenge: baby-re
+Team: hack.carleton (http://hack.carleton.team/)
+Runtime: ~8 minutes (single threaded E5-2650L v3 @ 1.80GHz on DigitalOcean)
+
+DigitalOcean is horrible for single threaded applications, I would highly suggest using something else.
+"""
+
+import angr
+
+proj = angr.Project('/defcon/baby-re',  load_options={'auto_load_libs': False})
+
+path_group = proj.factory.path_group(threads=4) # Doesn't really help to have more threads, but whatever.
+
+# If we get to 0x402941, "Wrong" is going to be printed out, so definitely avoid that.
+path_group.explore(find=0x40294b, avoid=0x402941) 
+# If you use anywhere before 0x40292c, angr won't have the flag to print out yet. So don't do that.
+
+print path_group.found[0].state.posix.dumps(1) # The flag is at the end.
+# The flag is: Math is hard!
+
+"""
+Note: There will be a bunch of warnings on your terminal that look like this.
+
+WARNING | 2016-05-21 17:34:33,185 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+WARNING | 2016-05-21 17:34:49,353 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+WARNING | 2016-05-21 17:35:11,810 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+WARNING | 2016-05-21 17:35:44,170 | simuvex.plugins.symbolic_memory | Concretizing symbolic length. Much sad; think about implementing.
+
+Don't worry about these, they're not an issue for this challenge.
+"""
+
+
