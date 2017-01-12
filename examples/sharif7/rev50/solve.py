f0a7dfc55d693ed8227ea782669b11940b212f4b
Jonathan Racicot <jonathan.racicot@gmail.com>
2017-01-12 17:43:43 -0500

    Fixed header, program name.
    Create a simple example on how to solve Rev50 from SharifCTF7.

--- examples/sharif7/rev50/solve.py
+++ examples/sharif7/rev50/solve.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# Copyright (C) 2017 Jonathan Racicot
+#!/usr/bin/python
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
