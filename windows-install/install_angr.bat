09600fe8b3ec4ea303557eabe1d0e8c75d52e197
Ben Cheney <ben.cheney@gmail.com>
2017-03-07 15:59:36 +1030

    Edits to Windows build/install process

--- windows-install/install_angr.bat
+++ windows-install/install_angr.bat
@@ -2,12 +2,18 @@
 
 powershell -Command "wget https://github.com/angr/archinfo/archive/master.zip -OutFile archinfo-master.zip"
 powershell -Command "wget https://github.com/angr/pyvex/archive/master.zip -OutFile pyvex-master.zip"
+powershell -Command "wget https://github.com/angr/cle/archive/master.zip -OutFile cle-master.zip"
+powershell -Command "wget https://github.com/angr/simuvex/archive/master.zip -OutFile simuvex-master.zip"
 powershell -Command "wget https://github.com/angr/angr/archive/master.zip -OutFile angr-master.zip"
-powershell -Command "Expand-Archive archinfo-master.zip ."
-powershell -Command "Expand-Archive pyvex-master.zip ."
-powershell -Command "Expand-Archive angr-master.zip ."
-
-pip install capstone-windows
+powershell -Command "wget https://github.com/unicorn-engine/unicorn/releases/download/1.0/unicorn-1.0.0-python-win32.msi -OutFile unicorn-python.msi"
+powershell -Command "wget https://github.com/unicorn-engine/unicorn/releases/download/1.0/unicorn-1.0-win32.zip -OutFile unicorn-core.zip"
+powershell -Command "wget https://github.com/aquynh/capstone/releases/download/3.0.5-rc2/capstone-3.0.5-rc2-python-win32.msi -OutFile capstone-python.msi"
+powershell -Command "Expand-Archive -Force archinfo-master.zip ."
+powershell -Command "Expand-Archive -Force pyvex-master.zip ."
+powershell -Command "Expand-Archive -Force cle-master.zip ."
+powershell -Command "Expand-Archive -Force simuvex-master.zip ."
+powershell -Command "Expand-Archive -Force angr-master.zip ."
+powershell -Command "Expand-Archive -Force unicorn-core.zip ."
 
 cd archinfo-master
 mv requirements.txt requirements.txt.bak
@@ -17,11 +23,28 @@ python setup.py install
 cd ..\pyvex-master
 mv requirements.txt requirements.txt.bak
 powershell -Command "wget https://raw.githubusercontent.com/Spirotot/angr_windows_install/master/requirements/pyvex_requirements.txt -OutFile requirements.txt"
-python setup.py install
+pip install -r requirements.txt
 python setup.py install
 
-cd ..\angr-master
+cd ..
+unicorn-python.msi
+if errorlevel 1 (
+    echo Warning: Unicorn installer failed!
+REM    exit /b %errorlevel%
+)
+python copy_unicorn_files.py unicorn-1.0-win32
+
+capstone-python.msi
+if errorlevel 1 (
+    echo Warning: Capstone installer failed!
+REM    exit /b %errorlevel%
+)
+
+pip install .\simuvex-master .\cle-master
+
+cd angr-master
 mv requirements.txt requirements.txt.bak
 powershell -Command "wget https://raw.githubusercontent.com/Spirotot/angr_windows_install/master/requirements/angr_requirements.txt -OutFile requirements.txt"
 pip install -r requirements.txt
-python setup.py install
+cd ..
+pip install .\angr-master
