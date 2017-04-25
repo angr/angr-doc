4e82499ca9e8dce3f4699e7630ce0c8efe67fbd4
Owlz <whootandahalf@gmail.com>
2017-04-24 22:43:15 -0400

    Adding simple_heap_overflow to the test script

--- examples/simple_heap_overflow/solve.py
+++ examples/simple_heap_overflow/solve.py
@@ -1,46 +1,71 @@
 #!/usr/bin/env python
 
-# angr now has the ability to correctly execute through malloc. this is a big
-# achievement as malloc is a complicated function and shows off how much effort
-# has been placed into creating correct execution. In this example, we will use
-# angr to perform a basic heap overwrite and achieve control over rip.
+import os, os.path
+import subprocess
 
-# The premise of this binary is to ask for two inputs, the second of which can
-# overflow into the area of the first. Further, a pointer will be dereferenced
-# in this process, thus giving us a target to control execution from.
+DIR = os.path.dirname(os.path.realpath(__file__))
 
-import angr, simuvex
+def main():
+    # angr now has the ability to correctly execute through malloc. this is a big
+    # achievement as malloc is a complicated function and shows off how much effort
+    # has been placed into creating correct execution. In this example, we will use
+    # angr to perform a basic heap overwrite and achieve control over rip.
+    
+    # The premise of this binary is to ask for two inputs, the second of which can
+    # overflow into the area of the first. Further, a pointer will be dereferenced
+    # in this process, thus giving us a target to control execution from.
 
-# By default, angr will use a sim procedure instead of going through malloc
-# This will tell angr to go ahead and use libc's calloc
-proj = angr.Project("./simple_heap_overflow", exclude_sim_procedures_list=["calloc"])
+    import angr, simuvex
+    
+    # By default, angr will use a sim procedure instead of going through malloc
+    # This will tell angr to go ahead and use libc's calloc
+    proj = angr.Project("./simple_heap_overflow", exclude_sim_procedures_list=["calloc"])
+    
+    # The extra option here is due to a feature not yet in angr for handling
+    # underconstraining 0 initialization of certain memory allocations
+    state = proj.factory.entry_state(add_options={simuvex.o.CGC_ZERO_FILL_UNCONSTRAINED_MEMORY})
+    
+    # We're looking for unconstrained paths, it means we may have control
+    pg = proj.factory.path_group(state,save_unconstrained=True)
 
-# The extra option here is due to a feature not yet in angr for handling
-# underconstraining 0 initialization of certain memory allocations
-state = proj.factory.entry_state(add_options={simuvex.o.CGC_ZERO_FILL_UNCONSTRAINED_MEMORY})
+    # Step execution until we find a place we may control
+    while len(pg.active) > 0 and len(pg.unconstrained) == 0:
+        pg.step()
 
-# We're looking for unconstrained paths, it means we may have control
-pg = proj.factory.path_group(state,save_unconstrained=True)
+    # In [9]: pg
+    # Out[9]: <PathGroup with 1 deadended, 1 unconstrained>
 
-# Step execution until we find a place we may control
-while len(pg.active) > 0 and len(pg.unconstrained) == 0:
-    pg.step()
+    # Make a copy of the state to play with
+    s = pg.unconstrained[0].state.copy()
 
-# In [9]: pg
-# Out[9]: <PathGroup with 1 deadended, 1 unconstrained>
+    # Now we can simply tell angr to set the instruction pointer to point at the
+    # win function to give us execution
+    s.add_constraints(s.regs.rip == proj.loader.main_bin.get_symbol('win').addr) 
 
-# Make a copy of the state to play with
-s = pg.unconstrained[0].state.copy()
+    assert s.satisfiable()
 
-# Now we can simply tell angr to set the instruction pointer to point at the
-# win function to give us execution
-s.add_constraints(s.regs.rip == proj.loader.main_bin.get_symbol('win').addr) 
+    # Call the solving engine and write the solution out to a file called "exploit"
+    print("Writing exploit as \"exploit\"")
+    s.posix.dump(0,"exploit")
 
-assert s.satisfiable()
+    # Now you can run the program and feed it your exploit to gain execution
+    # ./simple_heap_overflow < exploit
 
-# Call the solving engine and write the solution out to a file called "exploit"
-print("Writing exploit as \"exploit\"")
-s.posix.dump(0,"exploit")
+def test():
 
-# Now you can run the program and feed it your exploit to gain execution
-# ./simple_heap_overflow < exploit
+    # Generate the exploit
+    main()
+
+    # Make sure it worked
+    out = subprocess.check_output("{0} < {1}".format(
+        os.path.join(DIR,"simple_heap_overflow"),
+        os.path.join(DIR,"exploit"),
+        )
+        ,shell=True)
+
+    # Assert we got to the printing of Win
+    assert "Win" in out
+
+
+if __name__ == '__main__':                                                                                                                                                                                       
+    main()                                                                                                                                                                                                 
