8cfeee0da27cfed396e04b88a3dd4394317a3767
Audrey Dutcher <audrey@rhelmot.io>
2017-12-02 16:27:16 -0800

    Fix angrybird example; closes #166

--- examples/codegate_2017-angrybird/solve.py
+++ examples/codegate_2017-angrybird/solve.py
@@ -4,30 +4,32 @@
 # Runtime: ~3 minutes
 
 import angr
-import string
 
 START_ADDR = 0x4007c2
-FIND_ADDR = 0x404fc1  # This is shortly after the printf.
+FIND_ADDR = 0x404fab  # This is right before the printf
 
 def main():
-	proj = angr.Project('angrybird', load_options={"auto_load_libs": False})
+	proj = angr.Project('angrybird')
 	# There's a couple anti-run instructions in this binary.
 	# Yes, anti-run. That's not a typo.
 
 	# Because I'm not interested in fixing a weird binary, I'm going to skip all the beginning of the program.
+	# this also skips a bunch of initialization, so let's fix that:
 	state = proj.factory.entry_state(addr=START_ADDR)
+	state.regs.rbp = state.regs.rsp
+	# using the same values as the binary doesn't work for these variables, I think because they point to the GOT and the binary is using that to try to fingerprint that it's loaded in angr. Setting them to pointers to symbolic memory works fine.
+	state.mem[state.regs.rbp - 0x70].long = 0x1000
+	state.mem[state.regs.rbp - 0x68].long = 0x1008
+	state.mem[state.regs.rbp - 0x60].long = 0x1010
+	state.mem[state.regs.rbp - 0x58].long = 0x1018
 
 	sm = proj.factory.simulation_manager(state)  # Create the SimulationManager.
-
 	sm.explore(find=FIND_ADDR)  # This will take a couple minutes. Ignore the warning message(s), it's fine.
 	found = sm.found[-1]
-	stdin = found.posix.dumps(0)
+	flag = found.posix.dumps(0)
 
 	# This trims off anything that's not printable.
-	flag = filter(lambda x: x in string.printable, stdin).split()[0]
-
-	# (•_•) ( •_•)>⌐■-■ (⌐■_■)
-	return flag
+	return flag[:20]
 
 def test():
 	assert main() == 'Im_so_cute&pretty_:)'
