7e5ef6f8b6b2e72695e4613c838090dcb850a5b9
Audrey Dutcher <audrey@rhelmot.io>
2018-04-18 17:57:07 -0700

    Fix defcamp_r100 for new files

--- examples/defcamp_r100/solve.py
+++ examples/defcamp_r100/solve.py
@@ -1,14 +1,14 @@
 import angr
 
 def main():
-    p = angr.Project("r100", load_options={'auto_load_libs': False})
-    ex = p.surveyors.Explorer(find=(0x400844, ), avoid=(0x400855,))
-    ex.run()
+    p = angr.Project("r100")
+    simgr = p.factory.simulation_manager(p.factory.full_init_state())
+    simgr.explore(find=0x400844, avoid=0x400855)
 
-    return ex.found[0].posix.dumps(0).strip('\0\n')
+    return simgr.found[0].posix.dumps(0).strip('\0\n')
 
 def test():
-    assert main() == 'Code_Talkers'
+    assert main().startswith('Code_Talkers')
 
 if __name__ == '__main__':
     print main()
