e2a8ff3eb2d3c0a82628c3e0fac69a848f153fa6
Audrey Dutcher <audrey@rhelmot.io>
2018-04-20 23:26:59 -0700

    Move chdir into doctest_single

--- tests/test_gitbook.py
+++ tests/test_gitbook.py
@@ -22,53 +22,53 @@ example_dirs = filter(lambda s: '.' not in s, os.listdir(_path('examples')))
 sys.path.append('.')
 
 def doctest_single(md_file):
-    claripy.ast.base.var_counter = itertools.count()
-    lines = open(md_file).read().split('\n')
-    test_enabled = False
-    multiline_enabled = False
-    multiline_stuff = ''
-    env = {}
-
-    def try_running(line, i):
-        try:
-            exec line in env
-        except Exception as e:
-            print 'Error on line %d of %s: %s' % (i+1, md_file, e)
-            traceback.print_exc()
-            raise Exception('Error on line %d of %s: %s' % (i+1, md_file, e))
-
-    for i, line in enumerate(lines):
-        if test_enabled:
-            if line == '```':
-                test_enabled = False
-            else:
-                if not multiline_enabled:
-                    if line.startswith('>>> '):
-                        line = line[4:]
-                        if lines[i+1].startswith('... '):
-                            multiline_enabled = True
-                            multiline_stuff = line + '\n'
-                        else:
-                            try_running(line, i)
-                else:
-                    assert line.startswith('... ')
-                    line = line[4:]
-                    multiline_stuff += line + '\n'
-                    if not lines[i+1].startswith('... '):
-                        multiline_enabled = False
-                        try_running(multiline_stuff, i)
-        else:
-            if line == '```python':
-                test_enabled = True
-
-def test_docs():
     orig_path = os.getcwd()
     os.chdir(_path('.'))
     try:
-        for md_file in md_files:
-            yield doctest_single, md_file
+        claripy.ast.base.var_counter = itertools.count()
+        lines = open(md_file).read().split('\n')
+        test_enabled = False
+        multiline_enabled = False
+        multiline_stuff = ''
+        env = {}
+
+        def try_running(line, i):
+            try:
+                exec line in env
+            except Exception as e:
+                print 'Error on line %d of %s: %s' % (i+1, md_file, e)
+                traceback.print_exc()
+                raise Exception('Error on line %d of %s: %s' % (i+1, md_file, e))
+
+        for i, line in enumerate(lines):
+            if test_enabled:
+                if line == '```':
+                    test_enabled = False
+                else:
+                    if not multiline_enabled:
+                        if line.startswith('>>> '):
+                            line = line[4:]
+                            if lines[i+1].startswith('... '):
+                                multiline_enabled = True
+                                multiline_stuff = line + '\n'
+                            else:
+                                try_running(line, i)
+                    else:
+                        assert line.startswith('... ')
+                        line = line[4:]
+                        multiline_stuff += line + '\n'
+                        if not lines[i+1].startswith('... '):
+                            multiline_enabled = False
+                            try_running(multiline_stuff, i)
+            else:
+                if line == '```python':
+                    test_enabled = True
     finally:
         os.chdir(orig_path)
+
+def test_docs():
+    for md_file in md_files:
+        yield doctest_single, md_file
 
 if __name__ == '__main__':
     for tester, arg in test_docs():
