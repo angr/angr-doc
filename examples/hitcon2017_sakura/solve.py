96d86319b1a9aa8caea0243115ad1cae3f0941de
Kyle ZENG <jkjh1jkjh1@gmail.com>
2018-05-28 13:31:12 +0800

    add to test and not using pwntools for example hitcon_sakura

--- examples/hitcon2017_sakura/solve.py
+++ examples/hitcon2017_sakura/solve.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # Author: Kyle ZENG
-# Runtime: ~10 minutes
+# Runtime: ~6 minutes
 
 import hashlib
-
-from pwn import *
-context.arch = 'amd64'
-
 import angr
-import logging
-logging.getLogger('angr.sim_manager').setLevel(logging.DEBUG)
-
 
 def main():
-    e = ELF('./sakura')
+    e = open('./sakura', 'rb').read()
     
     # make sure return value is not 0, add corresponding addr to avoid list
     avoids = []
-    gen = e.search(asm('mov byte ptr [rbp-0x1E49], 0'))
-    try:
-        while True:
-            avoids.append(0x400000+gen.next())
-    except:
-        pass
+    index = 0
+    while True:
+        # avoid asm('mov byte ptr [rbp-0x1E49], 0')
+        index = e.find('\xc6\x85\xb7\xe1\xff\xff\x00', index+1)
+        if index == -1:
+            break
+        addr = 0x400000 + index
+        avoids.append(addr)
     
     # find list
     finds = []
-    gen = e.search(asm('mov rdi, rax'))
+    index = 0
     while True:
-        addr = 0x400000+gen.next()
-        gen.next()
-        if addr > 0x410FF5:
+        # find asm('mov rdi, rax')
+        index = e.find('H\x89\xc7', index+1)
+        if index == -1 or index > 0x10ff5:
             break
+        addr = 0x400000 + index
         finds.append(addr)
+
+        # skip a addr we don't want to find
+        index = e.find('H\x89\xc7', index+1)
 
     # initialize project
     proj = angr.Project('./sakura')
@@ -42,10 +41,8 @@ def main():
     
     # find ans stage by stage
     for find in finds:
-        print hex(find)
         simgr.explore(find=find, avoid=avoids)
         found = simgr.found[0]
-        print [found.posix.dumps(0)]
         simgr = proj.factory.simgr(found)
     
     # evaluate text
@@ -59,6 +56,9 @@ def test():
     assert main() == 'hitcon{6c0d62189adfd27a12289890d5b89c0dc8098bc976ecc3f6d61ec0429cccae61}'
 
 if __name__ == '__main__':
+    import logging
+    logging.getLogger('angr.sim_manager').setLevel(logging.DEBUG)
+
     print main()
 
 
