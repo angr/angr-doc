76fda7e2bf405ad90b0dc5fe09d0e01b7454c7ab
Audrey Dutcher <audrey@rhelmot.io>
2021-09-30 13:24:03 -0700

    un-harden insomnihack_aeg example binary; see #44

--- examples/insomnihack_aeg/demo_bin.c
+++ examples/insomnihack_aeg/demo_bin.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 char component_name[128] = {0};
 
@@ -20,9 +22,8 @@ comp_t *initialize_component(char *cmp_name) {
     cmp = malloc(sizeof(struct component));
     cmp->do_something = sample_func;
 
-    printf("Copying component name...\n"); 
-    while (*cmp_name)
-        cmp->name[i++] = *cmp_name++;
+    printf("Copying component name...\n");
+    strcpy(cmp->name, cmp_name);
 
     cmp->name[i] = '\0';
     return cmp;
@@ -39,5 +40,6 @@ int main(void)
     cmp = initialize_component(component_name);    
 
     printf("Running component...\n");
+    mprotect((void*)((long)&component_name & ~0xfff), 0x1000, PROT_READ | PROT_EXEC);
     cmp->do_something(1);
 }
