ca372a7f89352266d300fffbf87c214ee17cf406
ASUAD\aganes17 <adersh.g.97@gmail.com>
2021-12-19 23:20:23 -0700

    made function single line

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -155,9 +155,7 @@ def test_whitehatvn2015_re400():
 
 @slow_test
 @flaky(max_runs=3, min_passes=1)
-def test_secconquals2016_ropsynth():
-    exampletest_single("secconquals2016_ropsynth")
-    # technically not that slow but impossible to run under multiprocessing
+def test_secconquals2016_ropsynth(): exampletest_single('secconquals2016_ropsynth') # technically not that slow but impossible to run under multiprocessing
 
 
 @slow_test
