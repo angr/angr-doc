1793a65ecc7944f63275d340a3b274acf136b31a
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2022-05-18 12:24:35 -0700

    Remove generators in test_gitbook.py (#412)
    * Removed generators in test_gitbook.py
    
    * Ignored few lint errors.
    
    * Few changes in test_gitbook.py
    
    * Removed lint errors in test_gitbook.py
    
    * Fixed consider-using-with lint error in test_gitbook.py
    
    * Fixing lint errors in test_gitbook.py
    
    * Made changes as per the feedback.
    
    * Made changes as per the feedback.
    
    * Fixed lint errors in test_gitbook.py

--- tests/test_gitbook.py
+++ tests/test_gitbook.py
@@ -1,71 +1,75 @@
 # pylint: disable=exec-used
 import os
+import unittest
 import sys
 import traceback
 import itertools
 import claripy
 
-filepath = __file__
+# pylint: disable=missing-class-docstring, no-self-use
+class TestGitbook(unittest.TestCase):
 
-def _path(d):
-    return os.path.join(os.path.dirname(filepath), '..', d)
+    def setUp(self):
+        self.filepath = __file__
 
-md_files = []
-for _p in ('docs', 'docs/analyses', 'docs/courses'):
-    md_files += [os.path.join(_p, t) for t in os.listdir(_path(_p)) if t.endswith('.md')]
-example_dirs = [s for s in os.listdir(_path('examples')) if '.' not in s]
+        self.md_files = []
+        for _p in ('docs', 'docs/analyses', 'docs/courses'):
+            self.md_files += [os.path.join(_p, t) for t in os.listdir(self._path(_p)) if t.endswith('.md')]
 
-sys.path.append('.')
+    def _path(self, d):
+        return os.path.join(os.path.dirname(self.filepath), '..', d)
 
-def doctest_single(md_file):
-    orig_path = os.getcwd()
-    os.chdir(_path('.'))
-    try:
-        claripy.ast.base.var_counter = itertools.count()
-        lines = open(md_file, encoding='utf-8').read().split('\n')
-        test_enabled = False
-        multiline_enabled = False
-        multiline_stuff = ''
-        env = {}
+    def doctest_single(self, md_file):
+        orig_path = os.getcwd()
+        os.chdir(self._path('.'))
+        try:
+            claripy.ast.base.var_counter = itertools.count()
+            test_enabled = False
+            multiline_enabled = False
+            multiline_stuff = ''
+            env = {}
 
-        def try_running(line, i):
-            try:
-                exec(line, env)
-            except Exception as e:
-                print('Error on line %d of %s: %s' % (i+1, md_file, e))
-                traceback.print_exc()
-                raise Exception('Error on line %d of %s: %s' % (i+1, md_file, e))
+            def try_running(line, i):
+                try:
+                    exec(line, env)
+                except Exception as e:
+                    print('Error on line %d of %s: %s' % (i+1, md_file, e))
+                    traceback.print_exc()
+                    raise Exception('Error on line %d of %s: %s' % (i+1, md_file, e)) from e
 
-        for i, line in enumerate(lines):
-            if test_enabled:
-                if line == '```':
-                    test_enabled = False
-                else:
-                    if not multiline_enabled:
-                        if line.startswith('>>> '):
-                            line = line[4:]
-                            if lines[i+1].startswith('... '):
-                                multiline_enabled = True
-                                multiline_stuff = line + '\n'
+            with open(md_file,"r", encoding='utf-8') as file:
+                lines = [line.rstrip('\n') for line in file]
+                for i, line in enumerate(lines):
+                    if test_enabled:
+                        if line == '```':
+                            test_enabled = False
+                        else:
+                            if not multiline_enabled:
+                                if line.startswith('>>> '):
+                                    line = line[4:]
+                                    if lines[i+1].startswith('... '):
+                                        multiline_enabled = True
+                                        multiline_stuff = line + '\n'
+                                    else:
+                                        try_running(line, i)
                             else:
-                                try_running(line, i)
+                                assert line.startswith('... ')
+                                line = line[4:]
+                                multiline_stuff += line + '\n'
+                                if not lines[i+1].startswith('... '):
+                                    multiline_enabled = False
+                                    try_running(multiline_stuff, i)
                     else:
-                        assert line.startswith('... ')
-                        line = line[4:]
-                        multiline_stuff += line + '\n'
-                        if not lines[i+1].startswith('... '):
-                            multiline_enabled = False
-                            try_running(multiline_stuff, i)
-            else:
-                if line == '```python':
-                    test_enabled = True
-    finally:
-        os.chdir(orig_path)
+                        if line == '```python':
+                            test_enabled = True
+        finally:
+            os.chdir(orig_path)
 
-def test_docs():
-    for md_file in md_files:
-        yield doctest_single, md_file
+    def test_docs(self):
+        sys.path.append('.')
+        for md_file in self.md_files:
+            self.doctest_single(md_file)
+        sys.path.pop()
 
 if __name__ == '__main__':
-    for tester, arg in test_docs():
-        tester(arg)
+    unittest.main()
