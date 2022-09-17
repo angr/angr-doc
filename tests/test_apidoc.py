33efb7a3a61283142c816f6825e8bc36cc1f5607
Audrey Dutcher <audrey@rhelmot.io>
2022-09-16 23:19:05 -0700

    Blacklist the peephole optimizations module from the api doc (#431)

--- tests/test_apidoc.py
+++ tests/test_apidoc.py
@@ -11,8 +11,10 @@ def test_api_coverage():
     exclude = ["angr.tablespecs", "angr.service", "angr.analyses.girlscout", "pyvex.vex_ffi",
                "claripy.backends.remotetasks", "claripy.backends.backendremote", "cle.backends.idabin"]
     exclude_prefix = ["angr.procedures", "angr.analyses.identifier", "angr.misc", "angr.protos", "angr.engines.vex",
-                      "angr.engines.soot", "claripy.utils", "cle.backends.externs.simdata",
-                      "angr.knowledge_plugins.sync", "pyvex.lifting.gym"]
+        "angr.engines.soot", "claripy.utils", "cle.backends.externs.simdata",
+        "angr.knowledge_plugins.sync", "pyvex.lifting.gym",
+        "angr.analyses.decompiler.peephole_optimizations",
+    ]
     for module in ["angr", "claripy", "cle", "pyvex", "archinfo", "ailment"]:
         docs_file = _path("api-doc/source/%s.rst" % module)
         module_dir = _path("../%s/%s" % (module, module))
